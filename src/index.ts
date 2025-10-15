cd62c09992ffcf8413a6b5ded2db36eeebdcd55c
elzuzu <45237180+elzuzu@users.noreply.github.com>
2025-10-15 17:32:16 +0200

    Add compliance metrics tracker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { ConfigManager, type ConfigManagerOptions, type ConfigSnapshot } from './core/configManager.js';
+export { MetricsTracker, type MetricsSnapshot, type TradeSample } from './ops/metrics.js';
